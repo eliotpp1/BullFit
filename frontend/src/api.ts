3385f8768b567b345f1d8fa59d9208b99781cc39
Eliot Pouplier <148195936+eliotpp1@users.noreply.github.com>
2024-06-23 17:13:02 +0200

    Delete frontend directory

--- frontend/src/api.ts
+++
@@ -1,6 +0,0 @@
-// frontend/src/api.ts
-export const fetchBackendMessage = async (): Promise<string> => {
-  const response = await fetch("/api");
-  const data = await response.text();
-  return data;
-};
