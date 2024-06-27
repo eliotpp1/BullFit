96db2b8b523f25eca5e4938290d86b40e8114837
eliotpp1 <eliot.pouplier@gmail.com>
2024-06-27 19:08:08 +0200

    feature: v2 calculator

--- front/src/components/calculator.js
+++ front/src/components/calculator.js
@@ -1,9 +1,6 @@
-// running calculator.js component
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Calculator = () => {
-  const navigate = useNavigate();
   const [distance, setDistance] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -11,16 +8,19 @@ const Calculator = () => {
   const [speed, setSpeed] = useState(0);
   const [pace, setPace] = useState(0);
   const [weight, setWeight] = useState(0);
+  const [distance2, setDistance2] = useState(0);
   const [calories, setCalories] = useState(0);
+  const [weight2, setWeight2] = useState(0);
   const [height, setHeight] = useState(0);
-  const [mas, setMas] = useState(0);
   const [restingHR, setRestingHR] = useState(0);
   const [maximalHR, setMaximalHR] = useState(0);
-  const [zone1, setZone1] = useState(0);
-  const [zone2, setZone2] = useState(0);
-  const [zone3, setZone3] = useState(0);
-  const [zone4, setZone4] = useState(0);
-  const [zone5, setZone5] = useState(0);
+  const [mas, setMas] = useState(0);
+  const [fc60, setFc60] = useState(0);
+  const [fc65, setFc65] = useState(0);
+  const [fc75, setFc75] = useState(0);
+  const [fc85, setFc85] = useState(0);
+  const [fc95, setFc95] = useState(0);
+  const [fc100, setFc100] = useState(0);
 
   const onChangeDistance = (e) => {
     setDistance(e.target.value);
@@ -28,37 +28,52 @@ const Calculator = () => {
 
   const calculateAvgSpeed = (event) => {
     event.preventDefault();
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    const totalSeconds = hours * 3600 + minutes * 60 + parseInt(seconds);
     const speed = distance / (totalSeconds / 3600);
-    const pace = totalSeconds / distance;
+    const pace = totalSeconds / 60 / distance;
     setSpeed(speed.toFixed(2));
     setPace(pace.toFixed(2));
+    localStorage.setItem("speed", speed);
+    localStorage.setItem("pace", pace);
   };
 
   const calculateCalories = (event) => {
     event.preventDefault();
-    const calories = 1.036 * weight * distance;
-    setCalories(calories.toFixed(2));
+    const caloriesBurned = 1.036 * weight * distance2;
+    setCalories(caloriesBurned.toFixed(2));
+    localStorage.setItem("calories", caloriesBurned);
   };
 
   const calculateMAS = (event) => {
     event.preventDefault();
-    const mas = (1000 * distance) / (height * height);
+    const mas = weight2 / ((height / 100) * (height / 100));
     setMas(mas.toFixed(2));
+    localStorage.setItem("mas", mas);
   };
 
   const calculateHRZ = (event) => {
     event.preventDefault();
-    const zone1 = 0.5 * (maximalHR - restingHR) + restingHR;
-    const zone2 = 0.6 * (maximalHR - restingHR) + restingHR;
-    const zone3 = 0.7 * (maximalHR - restingHR) + restingHR;
-    const zone4 = 0.8 * (maximalHR - restingHR) + restingHR;
-    const zone5 = 0.9 * (maximalHR - restingHR) + restingHR;
-    setZone1(zone1.toFixed(2));
-    setZone2(zone2.toFixed(2));
-    setZone3(zone3.toFixed(2));
-    setZone4(zone4.toFixed(2));
-    setZone5(zone5.toFixed(2));
+
+    // Assuming maximalHR and restingHR are state variables or defined elsewhere
+    // Convert restingHR to a number
+    const restingHRNumber = parseFloat(restingHR); // Use parseFloat to handle decimal numbers if needed
+
+    if (!isNaN(maximalHR) && !isNaN(restingHRNumber)) {
+      const fcReserve = maximalHR - restingHRNumber;
+
+      // Calculate and set each heart rate zone
+      setFc60(Math.round(restingHRNumber + 0.6 * fcReserve));
+      setFc65(Math.round(restingHRNumber + 0.65 * fcReserve));
+      setFc75(Math.round(restingHRNumber + 0.75 * fcReserve));
+      setFc85(Math.round(restingHRNumber + 0.85 * fcReserve));
+      setFc95(Math.round(restingHRNumber + 0.95 * fcReserve));
+      setFc100(maximalHR);
+      localStorage.setItem("fc65", fc65);
+      localStorage.setItem("fc75", fc75);
+      localStorage.setItem("fc85", fc85);
+      localStorage.setItem("fc95", fc95);
+      localStorage.setItem("fc100", fc100);
+    }
   };
 
   return (
@@ -67,6 +82,7 @@ const Calculator = () => {
         <h1>Running Calculator</h1>
         <p>by BullFit</p>
       </div>
+
       <div>
         <h2>Average speed and pace calculator</h2>
         <form onSubmit={calculateAvgSpeed}>
@@ -113,7 +129,7 @@ const Calculator = () => {
       </div>
 
       <div>
-        <h2> Calories burn during race </h2>
+        <h2>Calories burned during race</h2>
         <form onSubmit={calculateCalories}>
           <label htmlFor="weight">Weight (in kg)</label>
           <input
@@ -123,13 +139,13 @@ const Calculator = () => {
             value={weight}
             onChange={(e) => setWeight(e.target.value)}
           />
-          <label htmlFor="distance">Distance (in km)</label>
+          <label htmlFor="distance2">Distance (in km)</label>
           <input
             type="number"
-            id="distance"
-            name="distance"
-            value={distance}
-            onChange={(e) => setDistance(e.target.value)}
+            id="distance2"
+            name="distance2"
+            value={distance2}
+            onChange={(e) => setDistance2(e.target.value)}
           />
           <button type="submit">Calculate</button>
         </form>
@@ -138,16 +154,17 @@ const Calculator = () => {
         <h3>Results</h3>
         <p>Calories burned: {calories}</p>
       </div>
+
       <div>
         <h2>Calculate your MAS</h2>
         <form onSubmit={calculateMAS}>
-          <label htmlFor="weight">Weight (in kg)</label>
+          <label htmlFor="weight2">Weight (in kg)</label>
           <input
             type="number"
-            id="weight"
-            name="weight"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
+            id="weight2"
+            name="weight2"
+            value={weight2}
+            onChange={(e) => setWeight2(e.target.value)}
           />
           <label htmlFor="height">Height (in cm)</label>
           <input
@@ -164,8 +181,9 @@ const Calculator = () => {
         <h3>Results</h3>
         <p>MAS: {mas}</p>
       </div>
+
       <div>
-        <h2>Calculate your heart rate zone</h2>
+        <h2>Calculate your heart rate zones</h2>
         <form onSubmit={calculateHRZ}>
           <label htmlFor="restingHR">Resting heart rate</label>
           <input
@@ -189,35 +207,50 @@ const Calculator = () => {
       <div>
         <h3>Results</h3>
         <table>
-          <tr>
-            <th>Zone</th>
-            <th>Percentage</th>
-          </tr>
-          <tr>
-            <td>Zone 1</td>
-            <td>50-60%</td>
-            <td>{zone1}</td>
-          </tr>
-          <tr>
-            <td>Zone 2</td>
-            <td>60-70%</td>
-            <td>{zone2}</td>
-          </tr>
-          <tr>
-            <td>Zone 3</td>
-            <td>70-80%</td>
-            <td>{zone3}</td>
-          </tr>
-          <tr>
-            <td>Zone 4</td>
-            <td>80-90%</td>
-            <td>{zone4}</td>
-          </tr>
-          <tr>
-            <td>Zone 5</td>
-            <td>90-100%</td>
-            <td>{zone5}</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Zone</th>
+              <th>Percentage</th>
+              <th>Heart rate range</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>60-65%</td>
+              <td>
+                {fc60}-{fc65}
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>65-75%</td>
+              <td>
+                {fc65}-{fc75}
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>75-85%</td>
+              <td>
+                {fc75}-{fc85}
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>85-95%</td>
+              <td>
+                {fc85}-{fc95}
+              </td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>95-100%</td>
+              <td>
+                {fc95}-{fc100}
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </>
