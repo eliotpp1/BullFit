7fd2bc80370880593286359ab722eff55126427d
eliotpp1 <eliot.pouplier@gmail.com>
2024-07-01 12:55:25 +0200

    feature: dashboard v1

--- back/routes/users.js
+++ back/routes/users.js
@@ -2,22 +2,38 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// GET all users
+// GET tous les utilisateurs
 router.get("/", (req, res) => {
   User.find()
     .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json("Erreur : " + err));
 });
 
-// POST a new user
+// POST un nouvel utilisateur
 router.post("/add", (req, res) => {
   const { username, password } = req.body;
   const newUser = new User({ username, password });
 
   newUser
     .save()
-    .then(() => res.json("User added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then(() => res.json("Utilisateur ajouté !"))
+    .catch((err) => res.status(400).json("Erreur : " + err));
+});
+
+// GET toutes les données de l'utilisateur
+router.get("/getData", async (req, res) => {
+  const username = req.query.username; // Récupère le nom d'utilisateur à partir de la chaîne de requête
+  if (!username) return res.status(400).send("Le nom d'utilisateur est requis");
+
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) return res.status(404).send("Utilisateur non trouvé");
+    res.send(user);
+  } catch (err) {
+    res
+      .status(500)
+      .send("Erreur lors de la récupération des données de l'utilisateur");
+  }
 });
 
 module.exports = router;
--- front/src/components/dashboard.js
+++ front/src/components/dashboard.js
@@ -1,11 +1,12 @@
-// dashboard component
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./context/authcontext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Dashboard = () => {
   const { username, connected } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     if (!connected) {
@@ -13,10 +14,89 @@ const Dashboard = () => {
     }
   }, [connected, navigate]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!username) return;
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/users/getData?username=${username}`
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des données du tableau de bord :",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="dashboard">
-      <h3 className="dashboard__title">Dashboard</h3>
-      <p className="dashboard__text">Welcome, {username}!</p>
+      <h3 className="dashboard__title">Tableau de bord</h3>
+      <p className="dashboard__text">Bienvenue, {username}!</p>
+      <p className="dashboard__text">Vos données :</p>
+      {data ? (
+        <ul className="dashboard__list">
+          <li className="dashboard__list-item">ID: {data._id}</li>
+          <li className="dashboard__list-item">
+            Nom d'utilisateur: {data.username}
+          </li>
+          <li className="dashboard__list-item">
+            Date de création: {new Date(data.date).toLocaleString()}
+          </li>
+          {data.stats && (
+            <>
+              <li className="dashboard__list-item">
+                Mas: {JSON.stringify(data.stats.mas)}
+              </li>
+              <li className="dashboard__list-item">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Zone</th>
+                      <th>Pourcentage</th>
+                      <th>Plage de fréquence cardiaque</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>60-65%</td>
+                      <td>{`${data.stats.fc60} - ${data.stats.fc65}`}</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>65-75%</td>
+                      <td>{`${data.stats.fc65} - ${data.stats.fc75}`}</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>75-85%</td>
+                      <td>{`${data.stats.fc75} - ${data.stats.fc85}`}</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>85-95%</td>
+                      <td>{`${data.stats.fc85} - ${data.stats.fc95}`}</td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>95-100%</td>
+                      <td>{`${data.stats.fc95} - ${data.stats.fc100}`}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </li>
+            </>
+          )}
+        </ul>
+      ) : (
+        <p className="dashboard__text">Aucune donnée disponible</p>
+      )}
     </div>
   );
 };
